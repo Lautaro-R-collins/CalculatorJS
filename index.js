f47625f19424a02b540eb4f1e698ac2481531aae
Lautaro Rodriguez Collins <135086443+Lautaro-R-collins@users.noreply.github.com>
2024-05-30 00:20:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+//manejo del dom
+const pantalla = document.getElementById('pantalla')
+const botones = document.querySelectorAll('.btn')
+
+//variables de control
+let expresion = []
+let operadorActual = null 
+let operadorIngresado = false; 
+
+//eventos de los botones 
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const botonActivo = boton.textContent;
+
+        // Formatear pantalla
+        if (boton.id === 'clear') {
+            pantalla.textContent = '0';
+            expresion = [];
+            operadorActual = null;
+            operadorIngresado = false; 
+            return;
+        }
+
+        // Borrar
+        if (boton.id === 'borrar') {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === '0') {
+                pantalla.textContent = '0';
+                expresion = [];
+                operadorIngresado = false; 
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+                expresion.pop();
+                operadorIngresado = false; 
+            }
+            return;
+        }
+
+        // Mostrar números y operadores en pantalla y agregar a la expresión
+        if (!isNaN(botonActivo) || botonActivo === '.') {
+            if (pantalla.textContent === '0' && botonActivo !== '.') {
+                pantalla.textContent = botonActivo;
+            } else {
+                pantalla.textContent += botonActivo;
+            }
+
+            operadorIngresado = false; 
+
+            // Si el operadorActual está definido, estamos agregando al segundo número
+            if (operadorActual) {
+                if (!expresion[1]) {
+                    expresion[1] = botonActivo;
+                } else {
+                    expresion[1] += botonActivo;
+                }
+            } else {
+                if (!expresion[0]) {
+                    expresion[0] = botonActivo;
+                } else {
+                    expresion[0] += botonActivo;
+                }
+            }
+
+        } else if (
+            (botonActivo === '+' ||
+            botonActivo === '-' ||
+            botonActivo === 'x' ||
+            botonActivo === '%') && !operadorIngresado
+        ) {
+            operadorActual = botonActivo;
+            pantalla.textContent += botonActivo 
+            expresion[2] = operadorActual;
+            operadorIngresado = true; 
+        } else if (botonActivo === '=') {
+            calcularExpresion();
+            operadorActual = null;
+            operadorIngresado = false; 
+        }
+    });
+});
+
+//Función para manejar el cálculo de la expresión
+function calcularExpresion() {
+    let resultado = parseFloat(expresion[0])
+    let num2 = parseFloat(expresion[1])
+
+    if (isNaN(resultado) || isNaN(num2)) return
+
+    resultado = realizarOperacion(resultado, num2, expresion[2])
+
+    pantalla.textContent = resultado
+    expresion = [resultado.toString()]
+}
+
+//Funcion para realizar las operaciones matematicas
+function realizarOperacion(num1, num2, operador) {
+    switch (operador) {
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '%':
+            return num1 / num2
+        case 'x':
+            return num1 * num2
+        default:
+            return num1
+    }
+}
